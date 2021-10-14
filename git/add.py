fbeab5cd872241a67890354baa8fe3713d39f2ff
Troels Bjørnskov <troels.bjoernskov@teliacompany.com>
2021-10-14 18:31:33 +0200

    Refine git stage hunk command
    This commit refines the stage by hunk command by increasing the resolution and
    allowing for smaller hunks.

--- git/add.py
+++ git/add.py
@@ -46,47 +46,206 @@ class GitAddChoiceCommand(GitStatusCommand):
 
 
 class GitAddSelectedHunkCommand(GitTextCommand):
-    def run(self, edit):
-        self.run_command(['git', 'diff', '--no-color', '-U1', self.get_file_name()], self.cull_diff)
+    def calculate_header(self, start_before: int, start_after: int,
+                         plus_lines: int, minus_lines: int,
+                         context_before: bool, context_after: bool,
+                         buf_length: int, context_lines: int,
+                         first_line_is_delete: bool) -> str:
+
+        context = context_lines
+        if context_before == True:
+            context += 1
+        if context_after == True:
+            context += 1
+
+        if start_before == 0 or start_before ==1:
+            first = 1
+            third = 1
+        elif buf_length == start_after + plus_lines - 1 or buf_length == start_after:
+            first = start_before if minus_lines == 0 else start_before - 1
+            third = start_after if plus_lines == 0 else start_after - 1
+        else:
+            first = start_before if first_line_is_delete is False else start_before - 1
+            third = start_after if plus_lines == 0 else start_after - 1
+
+        second = minus_lines + context
+        fourth = plus_lines + context
+
+        if second == 0:
+            header = f'@@ -{first} +{third},{fourth} @@'
+        elif fourth == 0:
+            header = f'@@ -{first},{second} +{third} @@'
+        else:
+            header = f'@@ -{first},{second} +{third},{fourth} @@'
+
+        return header
+
+
+    def run(self, _):
+        self.run_command(['git', 'diff', '--no-color', '-U0', self.get_file_name()], self.cull_diff)
 
     def cull_diff(self, result):
-        selection = []
-        for sel in self.view.sel():
-            selection.append({
-                "start": self.view.rowcol(sel.begin())[0] + 1,
-                "end": self.view.rowcol(sel.end())[0] + 1,
-            })
+        buf = self.view
+
+        # lines are 1-based but we have trailing newline set
+        # which cancels each other out
+        buf_length, _ = buf.rowcol(buf.size())
 
         hunks = [{"diff": ""}]
         i = 0
         matcher = re.compile(r'^@@ -([0-9]*)(?:,([0-9]*))? \+([0-9]*)(?:,([0-9]*))? @@')
+        plus_lines = 0
+        minus_lines = 0
+        context_lines = 0
+        context_before = False
+        context_after = False
         for line in result.splitlines():
+            match = None
             if line.startswith('@@'):
+
+                # new hunk, so we record the length of the old and reset the counters
+                # but not the first iter since that contains the diff header
+                if i > 0:
+                    hunks[i]['plus_lines'] = plus_lines
+                    hunks[i]['minus_lines'] = minus_lines
+                    hunks[i]['context_lines'] = context_lines
+                    plus_lines = 0
+                    minus_lines = 0
+                    context_before = False
+                    context_after = False
+
                 i += 1
                 match = matcher.match(line)
-                start = int(match.group(3))
+
+                # first and third always exist
+                start_before = int(match.group(1))
+                first_line_is_delete = False if match.group(2) == '0' else True
+                start_after = int(match.group(3))
                 end = match.group(4)
+
+                # do not add context lines if we are in the beginning of the buffer
+                # or clause deals with deletes on second line
+                if start_after > 1 or (start_after == 1 and end and int(end) == 0):
+                    context_before = True
+
                 if end:
-                    end = start + int(end)
+                    if int(end) != 0:
+                        end = int(end) + start_after - 1
+                    else:
+                        end = int(end) + start_after
+
                 else:
-                    end = start
-                hunks.append({"diff": "", "start": start, "end": end})
-            hunks[i]["diff"] += line + "\n"
+                    end = start_after
+
+                # do not add context lines, if the diff affects the buffer end
+                # we exclude the final newline
+                # minus one means we exclude the final newline, and
+                # the plus one is because sublime's line index is 0-based
+
+                lines_of_buf = buf.rowcol((buf.size() -1))[0] + 1
+
+                if end < lines_of_buf:
+                    context_after = True
+
+                hunks.append({"diff": "", "minus_lines": minus_lines, "context_before": context_before, "context_after": context_after, "context_lines": 0, "start_before": start_before, "start_after": start_after, "plus_lines": plus_lines, "end": end, "first_line_is_delete": first_line_is_delete})
+            else:
+                if i > 0:
+                    if line.startswith('+'):
+                        plus_lines += 1
+                    elif line.startswith('-'):
+                        minus_lines += 1
+                hunks[i]["diff"] += line + "\n"
+
+        # when we are done with the loop, we store it in the dict.
+        hunks[i]['plus_lines'] = plus_lines
+        hunks[i]['minus_lines'] = minus_lines
+        hunks[i]['context_before'] = context_before
+        hunks[i]['context_after'] = context_after
 
         diffs = hunks[0]["diff"]
         hunks.pop(0)
-        selection_is_hunky = False
+        difflist = []
+        prev_next_line_num = -99
+        j = -1
         for hunk in hunks:
-            for sel in selection:
-                if sel["end"] < hunk["start"]:
-                    continue
-                if sel["start"] > hunk["end"]:
-                    continue
-                diffs += hunk["diff"]  # + "\n\nEND OF HUNK\n\n"
-                selection_is_hunky = True
+            hunk_start = int(hunk["start_after"])
+            hunk_end = int(hunk["end"])
+            for region in buf.sel():
 
-        if selection_is_hunky:
-            self.run_command(['git', 'apply', '--cached'], stdin=diffs)
+                sel_start = buf.rowcol(region.begin())[0] + 1
+                sel_end = buf.rowcol(region.end())[0] + 1
+
+                # Sublime's behavior is such that when a diff has only deletions, the next_modification
+                # command will move the caret to the line AFTER the change, thus our prevline becomes off-by-one.
+                # Only deletions means plus_lines == 0. Therefore, the red diff marker in the sublime
+                # gutter signifies plus_lines == 0.
+
+                if hunk['plus_lines'] == 0:
+                    if sel_end - 1 < hunk_start:
+                        continue
+                    elif sel_start - 1 > hunk_end:
+                        continue
+
+                    prev_line_offset = 1
+
+                else:
+                    if sel_end < hunk_start:
+                        continue
+                    elif sel_start > hunk_end:
+                        continue
+
+                    prev_line_offset = 2
+
+                prev_line_num_one_indexed = hunk_start - prev_line_offset + 1
+                # composing diffs
+                if prev_line_num_one_indexed == prev_next_line_num:
+
+                    # Icrease context lines by at least the binding line between
+                    difflist[j]['context_lines'] += 1
+
+                    difflist[j]['minus_lines'] += hunk['minus_lines']
+                    difflist[j]['plus_lines'] += hunk['plus_lines']
+
+                    if hunk['context_after'] == True:
+                        next_line = buf.substr(buf.full_line(buf.text_point(hunk_end,0)))
+                        difflist[j]['diff'] += hunk["diff"] + ' ' + next_line
+                    else:
+                        difflist[j]['diff'] += hunk["diff"]
+
+                    # Last hunk decides if the context after should be included
+                    difflist[j]['context_after'] = hunk['context_after']
+
+                    prev_next_line_num = hunk_end + 1
+                    continue
+
+                else:
+
+                    if hunk['context_before'] == True:
+                        prev_line_num = hunk_start - prev_line_offset
+                        prev_line = buf.substr(buf.full_line(buf.text_point(prev_line_num,0)))
+                        hunk["diff"] = ' ' + prev_line + hunk["diff"]
+
+                    if hunk['context_after'] == True:
+                        next_line = buf.substr(buf.full_line(buf.text_point(hunk_end,0)))
+                        hunk["diff"] += ' ' + next_line
+
+                    prev_next_line_num = hunk_end + 1
+                    difflist.append(hunk)
+                    j += 1
+                    break
+
+        if len(difflist) > 0:
+            for diff in difflist:
+                header = self.calculate_header(diff['start_before'], diff['start_after'],
+                                          diff['plus_lines'], diff['minus_lines'],
+                                          diff['context_before'], diff['context_after'],
+                                          buf_length, diff['context_lines'],
+                                          diff['first_line_is_delete'])
+                diffs += header + '\n' + diff["diff"]
+            # f = open("/user/patch", "w")
+            # f.write(diffs)
+            # f.close()
+            self.run_command(['git', 'apply', '--cached', '--ignore-space-change', '--ignore-whitespace'], stdin=diffs)
         else:
             sublime.status_message("No selected hunk")
 
